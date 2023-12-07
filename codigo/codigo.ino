7c1a8640070c3a78df45118d6850efae49c02c27
MateusSartorio <mateussartorio39@gmail.com>
2023-12-06 22:09:42 -0300

    codigo ajustado para robo que a gente ta construindo
    apenas motores funcionando ate agora

--- codigo/codigo.ino
+++ codigo/codigo.ino
@@ -1,25 +1,27 @@
 // Roda invertida eh a da esquerda
 
-#define PWM1_Ch 0
-#define PWM2_Ch 1
-#define PWM1_Res 8
+#define PWM_Ch1 0
+#define PWM_Ch2 1
+#define PWM_Ch3 2
+#define PWM_Ch4 3
 
-#define PWM1_Freq 1000
+#define PWM_Res 8
+#define PWM_Freq 1000
 
 #define CS_Sensors 29
 
 #define SLC 36
-#define SR 39
-#define SC 34
+#define SR 34
+#define SC 39
 #define SL 35
 #define SRC 32
 
 // #define MISO_ECHO 19
 
-#define ECH_1 33
-#define CHA_M1 25
-#define ECH_2 26
-#define CHA_M2 27
+#define M1_PWM1 23
+#define M1_PWM2 25
+#define M2_PWM1 26
+#define M2_PWM2 27
 
 #define RS 17
 #define E 16
@@ -55,181 +57,56 @@ void setup()
 {
   Serial.begin(115200);
 
-  /*
-  // put your setup code here, to run once:
-  pinMode(RS, OUTPUT);
-  pinMode(E, OUTPUT);
-  for (int i = 0; i < DATA_SIZE; i++){
-    pinMode(DATA[i], OUTPUT);
-  }
-  */
+  pinMode(M1_PWM1, OUTPUT);
+  pinMode(M1_PWM2, OUTPUT);
+  pinMode(M2_PWM1, OUTPUT);
+  pinMode(M2_PWM2, OUTPUT);
 
-  // initLCD();
+  ledcAttachPin(M1_PWM1, PWM_Ch1);
+  ledcAttachPin(M1_PWM2, PWM_Ch2);
+  ledcAttachPin(M2_PWM1, PWM_Ch3);
+  ledcAttachPin(M2_PWM2, PWM_Ch4);
 
-  /*
-  // Always good to clear and return home
-  write8bits(CLEAR_DISPLAY);
-  write8bits(RETURN_HOME);
-  delay(2); // 1.52ms delay needed for the Return Home command
-  writeData("Ain ze da manga");
-  */
+  ledcSetup(PWM_Ch1, PWM_Freq, PWM_Res);
+  ledcSetup(PWM_Ch2, PWM_Freq, PWM_Res);
+  ledcSetup(PWM_Ch3, PWM_Freq, PWM_Res);
+  ledcSetup(PWM_Ch4, PWM_Freq, PWM_Res);
 
-  pinMode(SLC, INPUT);
   pinMode(SR, INPUT);
-  pinMode(SC, INPUT);
-  pinMode(SL, INPUT);
-  pinMode(SRC, INPUT);
-  pinMode(CS_Sensors, OUTPUT);
-  digitalWrite(CS_Sensors, HIGH);
-  digitalWrite(ECH_1, HIGH);
-  digitalWrite(ECH_2, HIGH);
-
-  /*
-  pinMode(ECH_1, OUTPUT);
-  pinMode(CHA_M1, OUTPUT);
-  pinMode(ECH_2, OUTPUT);
-  pinMode(CHA_M2, OUTPUT);
-*/
-  /*
-  ledcAttachPin(CHA_M1, PWM1_Ch);
-  ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
-
-  ledcAttachPin(CHA_M2, PWM2_Ch);
-  ledcSetup(PWM2_Ch, PWM1_Freq, PWM1_Res);
-  */
 }
 
 void loop()
 {
-  // int input = analogRead(A0);
-  // int pwm = (int)(((float) input/1023.0)*255.0);
+  ledcWrite(PWM_Ch1, 0);
+  ledcWrite(PWM_Ch2, 255);
 
-  /*
-  unsigned long new_time = millis();
-  unsigned long delta_time = new_time - time;
-  if(delta_time >= 1000) {
-    time = new_time;
-    rpm = (int) (proportionality_constant*((float) position / (float) delta_time)*60.0);
-    position = 0;
-    Serial.println(rpm);
-  }
-  */
+  // direita
+  ledcWrite(PWM_Ch3, 0);
+  ledcWrite(PWM_Ch4, 255);
 
-  // leitura_sensor0 = analogRead(A1);
-  // leitura_sensor1 = analogRead(A2);
+  int input = analogRead(SR);
+  Serial.println(input);
 
-  // Serial.println(leitura_sensor0);
-  // Serial.println(leitura_sensor1);
-
-  // analogWrite(PWM1, pwm);
-  // analogWrite(PWM2, pwm);
-
-  /*
-  int slc = analogRead(SLC);
-  int sr = analogRead(SR);
-  int sc = analogRead(SC);
-  int sl = analogRead(SL);
-  int src = analogRead(SRC);
-  int echo = analogRead(MISO_ECHO);
-
-  digitalWrite(ECH_1, HIGH);
-  digitalWrite(ECH_2, HIGH);
-  ledcWrite(PWM1_Ch, 0);
-  ledcWrite(PWM2_Ch, 10000);
-
-  delay(2000);
-
-  Serial.print("slc");
-  Serial.print(slc);
-
-  Serial.print("sr");
-  Serial.print(sr);
-
-  Serial.print("sc");
-  Serial.print(sc);
-
-  Serial.print("sl");
-  Serial.print(sl);
-
-  Serial.print("src");
-  Serial.print(src);
-
-  Serial.print("echo");
-  Serial.print(echo);
-*/
-
-  /*
-    digitalWrite(CS_Sensors,HIGH);
-    digitalWrite(ECH_1, HIGH);
-    digitalWrite(ECH_2, HIGH);
-    ledcWrite(PWM1_Ch, 50);
-    ledcWrite(PWM2_Ch, 200);
-    delay(1000);
-    ledcWrite(PWM1_Ch, 200);
-    ledcWrite(PWM2_Ch, 50);
-    delay(1000);
-  */
-
-  int slc = analogRead(SLC);
-  int sr = analogRead(SR);
-  int sc = analogRead(SC);
-  int sl = analogRead(SL);
-  int src = analogRead(SRC);
-
-  int sr_anterior = 0;
-  int sc_anterior = 0;
-  int sl_anterior = 0;
-
-  int erro_esquerdo = 4095 - sl;
-  int erro_direito = 4095 - sr;
-
-  int pwm_esquerdo = 63;
-  int pwm_direito = 192;
-
-  // int pwm_esquerdo = -1*((int) ((float) kp*(erro_esquerdo))*(127.0/4095.0)) + 127;
-  // int pwm_direito = (int) ((float) kp*(erro_direito))*(127.0/4095.0);
-
-  /*
-  digitalWrite(ECH_1, HIGH);
-  digitalWrite(ECH_2, HIGH);
-  */
-
-  Serial.printf("sr: %d, src: %d, sc: %d, slc: %d, sl: %d", sr, src, sc, slc, sl);
-  // Serial.printf("pwd_dir: %d, pdw_esq: %d\n", pwm_esquerdo, pwm_direito);
-
-  /*
-  if(sl < 100) {
-    vira_direita(60);
-  }
-  else if(sr < 100) {
-    vira_esquerda(60);
-  }
-  else {
-    anda_reto(60);
-  }
-  */
-
-  delay(2000);
-  // writeData(buf);
+  delay(1000);
 }
 
-void anda_reto(int velocidade)
-{
-  ledcWrite(PWM1_Ch, 255 - velocidade);
-  ledcWrite(PWM2_Ch, velocidade);
-}
+// void anda_reto(int velocidade)
+// {
+//   ledcWrite(PWM1_Ch, 255 - velocidade);
+//   ledcWrite(PWM2_Ch, velocidade);
+// }
 
-void vira_esquerda(int velocidade)
-{
-  ledcWrite(PWM1_Ch, velocidade);
-  ledcWrite(PWM2_Ch, velocidade);
-}
+// void vira_esquerda(int velocidade)
+// {
+//   ledcWrite(PWM1_Ch, velocidade);
+//   ledcWrite(PWM2_Ch, velocidade);
+// }
 
-void vira_direita(int velocidade)
-{
-  ledcWrite(PWM1_Ch, 255 - velocidade);
-  ledcWrite(PWM2_Ch, 255 - velocidade);
-}
+// void vira_direita(int velocidade)
+// {
+//   ledcWrite(PWM1_Ch, 255 - velocidade);
+//   ledcWrite(PWM2_Ch, 255 - velocidade);
+// }
 
 /* ------------------------------------------------------
   Pulses the Enable pin to send data to the display
